d2947a1b05e8d4eec40c6fd0bc715669295e9085
Matthew <matthewtoro17@gmail.com>
2022-03-21 20:56:28 +0800

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+}
